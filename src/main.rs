c8ce36677835fcd0593a7392a9678e3af2e86ca2
Per Lindgren <per.lindgren@ltu.se>
2024-10-08 15:25:43 +0200

    now with ui

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,13 @@
 use avian2d::{math::*, prelude::*};
 use bevy::{diagnostic::FrameTimeDiagnosticsPlugin, prelude::*, window::WindowResolution};
-use bevy_bullet_hell::{block, camera, common::*, gamepad, overlay, player, shooting, tile};
+use bevy_bullet_hell::{block, camera, common::*, gamepad, overlay, player, shooting, tile, ui};
 use bevy_ecs_tilemap::prelude::*;
-// use bevy_egui::*;
-// use bevy_egui::EguiContext;
-use bevy::prelude::*;
-use bevy_egui::EguiContext;
-use bevy_egui::EguiPlugin;
-use bevy_inspector_egui::bevy_inspector;
-use bevy_inspector_egui::prelude::*;
-use bevy_inspector_egui::{egui, prelude::*, quick::WorldInspectorPlugin};
-use std::any::TypeId;
-
-#[derive(Component)]
-struct EditorWindow;
+use bevy_inspector_egui::DefaultInspectorConfigPlugin;
 
 fn setup(mut commands: Commands) {
     // we might want to setup a custom camera, for now just default
     commands.spawn(Camera2dBundle::default());
 }
-
-fn spawn_editor_window(mut commands: Commands) {
-    commands.spawn((Window::default(), EditorWindow));
-}
-
-fn inspector_ui(world: &mut World) {
-    let Ok(egui_context) = world
-        .query_filtered::<&mut EguiContext, With<EditorWindow>>()
-        .get_single(world)
-    else {
-        return;
-    };
-    let mut egui_context = egui_context.clone();
-
-    egui::Window::new("UI").show(egui_context.get_mut(), |ui| {
-        egui::ScrollArea::vertical().show(ui, |ui| {
-            // equivalent to `WorldInspectorPlugin`
-            bevy_inspector_egui::bevy_inspector::ui_for_world(world, ui);
-
-            egui::CollapsingHeader::new("Materials").show(ui, |ui| {
-                bevy_inspector_egui::bevy_inspector::ui_for_assets::<StandardMaterial>(world, ui);
-            });
-
-            ui.heading("Entities");
-            bevy_inspector_egui::bevy_inspector::ui_for_world_entities(world, ui);
-        });
-    });
-}
-
-// fn editor_ui(world: &mut World) {
-//     let mut egui_ctx = world
-//         .query_filtered::<&mut EguiContext, With<EditorWindow>>()
-//         .single(world)
-//         .clone();
-
-//     egui::CentralPanel::default().show(egui_ctx.get_mut(), |ui| {
-//         egui::ScrollArea::vertical()
-//             .auto_shrink([false, false])
-//             .show(ui, |ui| {
-//                 bevy_inspector::ui_for_world(world, ui);
-//             })
-//     });
-// }
 
 fn main() {
     App::new()
@@ -79,12 +25,11 @@ fn main() {
                 })
                 .set(ImagePlugin::default_nearest()),
             PhysicsPlugins::default().with_length_unit(1.0),
-            //WorldInspectorPlugin::new(),
             bevy_egui::EguiPlugin,
-            // DefaultInspectorConfigPlugin,
+            DefaultInspectorConfigPlugin,
+            FrameTimeDiagnosticsPlugin,
+            TilemapPlugin,
         ))
-        .add_plugins(TilemapPlugin)
-        .add_plugins(FrameTimeDiagnosticsPlugin)
         .insert_resource(ClearColor(Color::BLACK))
         .insert_resource(Gravity(Vector::ZERO))
         .add_event::<shooting::ShotEvent>()
@@ -97,13 +42,12 @@ fn main() {
                 block::setup,
                 shooting::setup,
                 tile::setup,
-                spawn_editor_window,
+                ui::setup,
             )
                 .chain(),
         )
         .add_systems(
             Update,
-            // (
             (
                 gamepad::update_system,
                 player::update_system,
@@ -114,10 +58,9 @@ fn main() {
                 shooting::collider_system,
                 overlay::fps_update_system,
                 camera::update_system,
-                inspector_ui,
+                ui::update_system,
             )
-                .chain(), // .before(shooting::new_shot_system),
+                .chain(),
         )
-        // )
         .run();
 }
