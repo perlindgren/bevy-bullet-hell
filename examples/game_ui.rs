8fe5f7e14c7d9fb650d2011973b6db68b26da0b3
Per Lindgren <per.lindgren@ltu.se>
2024-10-08 13:39:07 +0200

    game_ui cleanup

--- examples/game_ui.rs
+++ examples/game_ui.rs
@@ -1,20 +1,10 @@
 use avian2d::{math::*, prelude::*};
 use bevy::{diagnostic::FrameTimeDiagnosticsPlugin, prelude::*, window::WindowResolution};
-use bevy_bullet_hell::player::PlayerResource;
-use bevy_bullet_hell::{block, camera, common::*, gamepad, overlay, player, shooting, tile};
+use bevy_bullet_hell::{
+    block, camera, common::*, gamepad, overlay, player, player::PlayerResource, shooting, tile,
+};
 use bevy_ecs_tilemap::prelude::*;
-
-use bevy_inspector_egui::prelude::*;
 use bevy_inspector_egui::quick::ResourceInspectorPlugin;
-
-// `InspectorOptions` are completely optional
-#[derive(Reflect, Resource, Default, InspectorOptions)]
-#[reflect(Resource, InspectorOptions)]
-struct Configuration {
-    name: String,
-    #[inspector(min = 0.0, max = 1.0)]
-    option: f32,
-}
 
 fn setup(mut commands: Commands) {
     // we might want to setup a custom camera, for now just default
@@ -37,15 +27,11 @@ fn main() {
                 })
                 .set(ImagePlugin::default_nearest()),
             PhysicsPlugins::default().with_length_unit(1.0),
+            ResourceInspectorPlugin::<Time>::default(),
+            ResourceInspectorPlugin::<PlayerResource>::default(),
+            FrameTimeDiagnosticsPlugin,
+            TilemapPlugin,
         ))
-        .init_resource::<Configuration>() // `ResourceInspectorPlugin` won't initialize the resource
-        .register_type::<Configuration>() // you need to register your type to display it
-        .add_plugins(ResourceInspectorPlugin::<Configuration>::default())
-        // also works with built-in resources, as long as they are `Reflect`
-        .add_plugins(ResourceInspectorPlugin::<Time>::default())
-        .add_plugins(ResourceInspectorPlugin::<PlayerResource>::default())
-        .add_plugins(TilemapPlugin)
-        .add_plugins(FrameTimeDiagnosticsPlugin)
         .insert_resource(ClearColor(Color::BLACK))
         .insert_resource(Gravity(Vector::ZERO))
         .add_event::<shooting::ShotEvent>()
