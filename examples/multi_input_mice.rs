3c83d4d619b9d7cb44a57e7ab981df60b31ba393
Per Lindgren <per.lindgren@ltu.se>
2024-10-24 08:34:09 +0200

    multi input example works

--- examples/multi_input_mice.rs
+++ examples/multi_input_mice.rs
@@ -1,8 +1,14 @@
 // mouse events
 use bevy::prelude::*;
-use bevy_bullet_hell::{input_cfg, ui_egui_cfg};
+use bevy_bullet_hell::{common::NR_PLAYERS, input_cfg, ui_egui_cfg};
 use bevy_inspector_egui::DefaultInspectorConfigPlugin;
-use input_linux_tools::{device::*, mouse::*};
+use input_linux_tools::{
+    device::*,
+    input_linux::Key,
+    keyboard::{KeyStatus, KeyboardEvent},
+    mouse::*,
+};
+use std::collections::HashMap;
 
 fn main() {
     App::new()
@@ -29,17 +35,20 @@ fn main() {
         .run();
 }
 
-#[derive(Component)]
-pub struct Player(usize);
+#[derive(Component, Default)]
+pub struct Player {
+    id: usize,
+    keys_pressed: HashMap<Key, KeyStatus>,
+}
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
 
     let texture = asset_server.load("sprites/cross.png");
 
-    for i in 0..1 {
+    for i in 0..NR_PLAYERS {
         commands.spawn((
-            Player(i),
+            Player { id: i, ..default() },
             SpriteBundle {
                 texture: texture.clone(),
                 ..default()
@@ -51,31 +60,70 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
 pub fn update_system(
     time: Res<Time>,
     players_input_r: Res<input_cfg::PlayersInput>,
-    mut player_q: Query<(&mut Transform, &Player)>,
+    mut player_q: Query<(&mut Transform, &mut Player)>,
 ) {
-    let speed = 50.0;
-    let time_speed_delta = time.delta_seconds() * speed;
     for (index, input) in players_input_r.player_input.iter().enumerate() {
-        // if let Some(input) = &input.pos_input {
-        //     match input {
-        //         DeviceType::Mouse(mouse, _) => {
-        //             while let Some(event) = mouse.read() {
-        //                 match event {
-        //                     MouseEvent::MotionEvent(motion) => {
-        //                         if let Some((mut t, _)) =
-        //                             player_q.iter_mut().find(|(_t, Player(nr))| (*nr == index))
-        //                         {
-        //                             t.translation.x += motion.delta.x * time_speed_delta;
-        //                             t.translation.y -= motion.delta.y * time_speed_delta;
-        //                             // evdev delta in other direction
-        //                         }
-        //                     }
-        //                     _ => {}
-        //                 }
-        //             }
-        //         }
-        //         _ => {}
-        //     }
-        // }
+        match &input.pos_input.evdev {
+            Some(EvDev::Mouse(mouse)) => {
+                let speed = 50.0;
+                let time_speed_delta = time.delta_seconds() * speed;
+
+                // check if the player is connected to mouse
+                if let Some((mut t, _)) =
+                    player_q.iter_mut().find(|(_, player)| (player.id == index))
+                {
+                    // yes connected
+                    while let Some(event) = mouse.read() {
+                        match event {
+                            MouseEvent::MotionEvent(motion) => {
+                                t.translation.x += motion.delta.x * time_speed_delta;
+                                t.translation.y -= motion.delta.y * time_speed_delta;
+                                // evdev delta in other direction
+                            }
+                            _ => {}
+                        }
+                    }
+                }
+            }
+
+            Some(EvDev::Keyboard(keyboard)) => {
+                if let Some((mut t, mut player)) =
+                    player_q.iter_mut().find(|(_, player)| (player.id == index))
+                {
+                    // read events, and store their status
+                    while let Some(KeyboardEvent { key, status }) = keyboard.read() {
+                        player.keys_pressed.insert(key, status);
+                    }
+
+                    // match directions
+                    let left = player
+                        .keys_pressed
+                        .get(&Key::A)
+                        .map_or(false, |status| status == &KeyStatus::Pressed);
+                    let right = player
+                        .keys_pressed
+                        .get(&Key::D)
+                        .map_or(false, |status| status == &KeyStatus::Pressed);
+
+                    let up = player
+                        .keys_pressed
+                        .get(&Key::W)
+                        .map_or(false, |status| status == &KeyStatus::Pressed);
+                    let down = player
+                        .keys_pressed
+                        .get(&Key::S)
+                        .map_or(false, |status| status == &KeyStatus::Pressed);
+
+                    let speed = 400.0;
+                    let time_speed_delta = time.delta_seconds() * speed;
+                    //   update player position
+                    t.translation.y += time_speed_delta
+                        * (if up { 1.0 } else { 0.0 } - if down { 1.0 } else { 0.0 });
+                    t.translation.x += time_speed_delta
+                        * (if right { 1.0 } else { 0.0 } - if left { 1.0 } else { 0.0 });
+                }
+            }
+            _ => {}
+        }
     }
 }
